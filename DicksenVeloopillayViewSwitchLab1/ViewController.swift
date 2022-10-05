b755a9fadf7b815b8d3b76827b911320f4ca7945
Dicksen Veloopillay <dicksenveloopillay@Dicksens-MacBook-Pro.local>
2022-10-05 19:34:33 -0400

    initial commit

---
+++ DicksenVeloopillayViewSwitchLab1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DicksenVeloopillayViewSwitchLab1
+//
+//  Created by Dicksen Veloopillay on 2022-10-05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
